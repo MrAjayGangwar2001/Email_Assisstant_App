0a16a4b8b0896d8e3030804a755b5153de3ccce1
Ajay <gangwar030@gmail.com>
2025-12-17 13:42:08 +0530

    Frontend file

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Page from './components/Page'
+import SelectTone from './components/SelectTone'
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Page />
+      {/* <SelectTone /> */}
+
+    </>
+  )
+}
+
+export default App
