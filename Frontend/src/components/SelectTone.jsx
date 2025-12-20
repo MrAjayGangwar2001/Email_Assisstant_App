baa40862ab9fce70b5d6c823bec7c5f7d3c1ad7a
Ajay <gangwar030@gmail.com>
2025-12-20 19:07:21 +0530

    Some UI/UX Updates

--- Frontend/src/components/SelectTone.jsx
+++ Frontend/src/components/SelectTone.jsx
@@ -1,42 +1,30 @@
-import React, { useState } from 'react'
-// import * as React from 'react';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import { Container } from '@mui/material';
+import React from 'react'
+import {
+    Box,
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Select
+} from '@mui/material'
 
-function SelectTone({tone, setTone}) {
-
-    const handleChange = (e) => {
-        setTone(e.target.value);
-    };
-
-   
+function SelectTone({ tone, setTone }) {
     return (
-        <Container maxWidth="sm" sx={{ py: 1 }}>
-            <Box sx={{ minWidth: 120 }}>
-                <FormControl sx={{ my: 3, minWidth: 150, }}>
-                    <InputLabel id="demo-simple-select-label">Tone(optional)</InputLabel>
-                    <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                        value={tone}
-                        label="Tone"
-                        onChange={handleChange}
-                    >
-                        <MenuItem value="">None</MenuItem>
-                        <MenuItem value="Professional">Professional</MenuItem>
-                        <MenuItem value="Casual">Casual</MenuItem>
-                        <MenuItem value="Normal">Normal</MenuItem>
-                        <MenuItem value="Friendly">Friendly</MenuItem>
-                    </Select>
-                </FormControl>
-
-            </Box>
-            
-        </Container>
+        <Box sx={{ mt: 2 }}>
+            <FormControl fullWidth>
+                <InputLabel>Tone (optional)</InputLabel>
+                <Select
+                    value={tone}
+                    label="Tone (optional)"
+                    onChange={(e) => setTone(e.target.value)}
+                >
+                    <MenuItem value="">None</MenuItem>
+                    <MenuItem value="Professional">Professional</MenuItem>
+                    <MenuItem value="Casual">Casual</MenuItem>
+                    <MenuItem value="Friendly">Friendly</MenuItem>
+                    <MenuItem value="Formal">Formal</MenuItem>
+                </Select>
+            </FormControl>
+        </Box>
     )
 }
 
