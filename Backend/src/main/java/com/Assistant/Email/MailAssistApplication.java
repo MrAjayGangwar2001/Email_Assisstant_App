58ef1f15f68bc2249011367ad287491735c3feea
Ajay <gangwar030@gmail.com>
2025-12-22 02:49:26 +0530

    Main Class File

---
+++ Backend/src/main/java/com/Assistant/Email/MailAssistApplication.java
@@ -0,0 +1,14 @@
+package com.Assistant.Email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailAssistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailAssistApplication.class, args);
+		System.out.println("Email Assisstance Project is RUNNING.....");
+	}
+
+}
