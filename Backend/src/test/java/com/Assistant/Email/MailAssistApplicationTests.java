6b6d3d0b4a18b5653b4e6e40795073ed5a553235
Ajay <gangwar030@gmail.com>
2025-12-22 02:51:20 +0530

    Complete Backend of  Email Assistant App

---
+++ Backend/src/test/java/com/Assistant/Email/MailAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Assistant.Email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailAssistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
